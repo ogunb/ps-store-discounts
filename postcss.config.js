d6db4d63f5e5fe3d3df274044e4c2017b2c55690
Ogün Babacan <ogun.babacan@trendyol.com>
2019-06-13 13:49:39 +0300

    feat : PostCSS : Run PurgeCSS only on production build. feat : App.svelte : Add base for layout search w/ tailwindcss.

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,14 @@
 const tailwind = require('tailwindcss');
 const autoprefixer = require('autoprefixer');
 const PurgeCSS = require('@fullhuman/postcss-purgecss');
+const prod = process.env.NODE_ENV === 'production';
 
 module.exports = {
     plugins: [,
         tailwind,
         autoprefixer,
-        new PurgeCSS({
-            content: ['./src/**/*.js', './src/00/*.svelte'],
+        prod && new PurgeCSS({
+            content: ['./src/**/*.js', './src/**/*.svelte'],
             css: ['./src/*.css'],
             defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
         }),
--- src/App.svelte
+++ src/App.svelte
@@ -3,20 +3,33 @@
 
 	let searchResults = [];
 	let loading = true;
-	searchGames('days gone').then((res) => {
+	searchGames('south park').then((res) => {
 		searchResults = res
 		loading = false;
 	});
 </script>
 
-<div>
-	{#each searchResults as result}
-		<p>{result.attributes.name}</p>
-	{:else}
-		{#if loading}
-			<p>Aranıyor...</p>
+<div class="container">
+	<div class="flex flex-wrap-reverse justify-center">
+		{#each searchResults as result}
+			<div class="flex-grow flex-shrink rounded overflow-hidden shadow-lg w-1/5 mx-6 my-6 max-w-lg">
+				<img class="w-full" src={result.attributes['thumbnail-url-base'] + '?w=376&h=376'} alt="Sunset in the mountains">
+				<div class="px-6 py-4">
+					<div class="font-bold text-xl mb-2">{result.attributes.name}</div>
+					<p class="text-gray-700 text-base">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+					</p>
+				</div>
+				<div class="px-6 py-4">
+					<span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#action</span>
+				</div>
+			</div>
 		{:else}
-			<p>Sonuç Bulunamadı</p>
-		{/if}
-	{/each}
+			{#if loading}
+				<p>Aranıyor...</p>
+			{:else}
+				<p>Sonuç Bulunamadı</p>
+			{/if}
+		{/each}
+	</div>
 </div>
