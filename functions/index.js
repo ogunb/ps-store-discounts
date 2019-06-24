ce33e9c6587a2fd7fa916500b51133e3a75dea39
Ogün Babacan <ogun.babacan@trendyol.com>
2019-06-24 14:42:39 +0300

    refactor : functions : Use foreach instead of for loop. feat : package.json : Add firebase function scripts. refactor : FavoriteButton :  Use tailwind instead of css.

--- functions/index.js
+++ functions/index.js
@@ -2,13 +2,11 @@ const functions = require('firebase-functions');
 const glob = require('glob');
 
 const files = glob.sync('./*/*.*.js', { cwd: __dirname, ignore: './node_modules/**'});
-
-for(let i = 0; i < files.length; i++){
-    const filePath = files[i]
-    const filePathSplit = files[i].split('/');
+files.forEach(filePath => {
+    const filePathSplit = filePath.split('/');
     const functionTrigger = filePathSplit[1];
     const [ functionName, triggerType ] = filePathSplit[filePathSplit.length - 1].split('.');
     if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === functionName) {
         exports[functionName] = functions[functionTrigger][triggerType](require(filePath));
     }
-}
+});
